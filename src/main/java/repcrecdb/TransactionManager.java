4d2302c4f87f9de4dd148eccb77d00264899436e
iamnwi <iamnwi@gmail.com>
2019-11-09 22:36:49 -0500

    read and parse input from file or stdin

--- src/main/java/repcrecdb/TransactionManager.java
+++ src/main/java/repcrecdb/TransactionManager.java
@@ -1,6 +1,8 @@
 package repcrecdb;
 
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TransactionManager {
     ArrayList<DataManager> dms;
@@ -9,17 +11,71 @@ public class TransactionManager {
         this.dms = dms;
     }
 
-    public void run() {}
+    public void run(InputStream inputStream) {
+        Scanner input = new Scanner(inputStream);
+        try {
+            while (input.hasNext()) {
+                parse(input.nextLine());
+            }
+        } finally {
+            input.close();
+        }
+    }
 
-    public void parse() {}
+    public void parse(String instruction) {
+        // Parse command and args
+        String[] tokens = instruction.replaceAll("\\)", "").split("\\(");
+        String command = tokens[0];
+        String[] args = new String[0];
+        if (tokens.length > 1) {
+            args = tokens[1].replaceAll(" ", "").split(",");
+        }
 
-    public void begin() {}
+        // Dispatch
+        switch (command) {
+            case "begin":
+                if (args.length == 1)
+                    begin(args[0]);
+                break;
+            case "beginRO":
+                if (args.length == 1)
+                    beginRO(args[0]);
+                break;
+            case "read":
+                if (args.length == 2)
+                    read(args[0], args[1]);
+                break;
+            case "write":
+                if (args.length == 3)
+                    write(args[0], args[1], args[2]);
+                break;
+            case "dump":
+                dump();
+                break;
+            case "end":
+                if (args.length == 1)
+                    end(args[0]);
+                break;
+            case "fail":
+                if (args.length == 1)
+                    fail(args[0]);
+                break;
+            case "recover":
+                if (args.length == 1)
+                    recover(args[0]);
+                break;
+            default:
+                System.out.println("Unknown instruction");
+        }
+    }
 
-    public void beginRO() {}
+    public void begin(String transactionName) {}
 
-    public void read() {}
+    public void beginRO(String transactionName) {}
 
-    public void write() {}
+    public void read(String transactionName, String varName) {}
+
+    public void write(String transactionName, String varName, String val) {}
 
     public void dump() {
         for (DataManager dm : dms) {
@@ -27,11 +83,11 @@ public class TransactionManager {
         }
     }
 
-    public void end() {}
+    public void end(String transactionName) {}
 
-    public void fail() {}
+    public void fail(String siteName) {}
 
-    public void recover() {}
+    public void recover(String siteName) {}
 
     public void queryState() {
         System.out.println("Transaction Manager");
